03061141a081988cb7da02c15a68facca83504f7
Jatin Bhatnagar <jatin.cs22@bmsce.ac.in>
2026-01-05 23:16:10 +0530

    Initial submission

---
+++ main.py
@@ -0,0 +1,11 @@
+﻿from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/sayHello")
+def say_hello():
+    return {"message": "Hello User"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=80)
